2468c4bdc0047b16c4a0f6510e404e50be374744
lucasfarmwald <farmwaldlucas@gmail.com>
2021-10-18 19:35:50 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -2,6 +2,35 @@
  const plusBtn = document.getElementById('plus');
  const pauseBtn = document.getElementById('pause');
  const heartBtn = document.getElementById('heart');
+ const counter = document.getElementById('counter')
 
- 
- minusBtn.addEventListener('click',function(){})
+
+
+// starts timer when page is loaded
+
+function startTimer(){
+    counter.innerText = parseInt(counter.innerText) + 1
+    counter
+}
+
+
+ // subtract 1 to the counter when the "minus" button is clicked.
+
+ minusBtn.addEventListener('click',function(e){
+     counter.innerText -= 1
+    console.log(counter)
+ })
+
+ // add 1 to the counter when clicking on the "plus" button.
+
+ plusBtn.addEventListener('click',function(e){
+    counter.innerText = parseInt(counter.innerText) + 1
+   console.log(typeof counter.innerText)
+ })
+
+// pause the counter when the "pause" button is clicked.
+
+pauseBtn.addEventListener('click',function (){
+    counter.innerText =
+    console.log(pauseBtn)
+})
